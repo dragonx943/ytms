416c4d984fb4d106426f7b0ab31afaa41fe08b7d
Draken <131387159+dragonx943@users.noreply.github.com>
2024-05-27 22:58:03 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,15 @@
 function ytms {
-        $ytms_input = """$args[0]"""
-        $ytms_url = yt-dlp --format ba --get-url --extractor-args youtube:player_client=android,web ytsearch1:$ytms_input
-        $ytms_title = yt-dlp --get-title --extractor-args youtube:player_client=android,web ytsearch1:$ytms_input
-        $ytms_uploader = yt-dlp --print "%(uploader)s" --extractor-args youtube:player_client=android,web ytsearch1:$ytms_input
-        mpv --no-video --term-playing-msg="`r`nTiêu đề: $($ytms_title)`r`nNgười đăng tải: $($ytms_uploader)`r`n" $ytms_url
+    param (
+        [string]$ytms_input
+    )
+
+    if (-not $ytms_input) {
+        $ytms_input = Read-Host "Nhập từ khóa tìm kiếm trên YouTube"
+    }
+
+    $ytms_url = yt-dlp --format ba --get-url --extractor-args youtube:player_client=ios,web ytsearch1:$ytms_input
+    $ytms_title = yt-dlp --get-title --extractor-args youtube:player_client=ios,web ytsearch1:$ytms_input
+    $ytms_uploader = yt-dlp --print "%(uploader)s" --extractor-args youtube:player_client=ios,web ytsearch1:$ytms_input
+
+    mpv --no-video --term-playing-msg="`r`nTiêu đề: $($ytms_title)`r`nĐăng tải bởi: $($ytms_uploader)`r`n" $ytms_url
 }
